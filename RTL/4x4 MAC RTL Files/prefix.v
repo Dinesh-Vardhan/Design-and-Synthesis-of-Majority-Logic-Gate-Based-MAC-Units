82e25c137866e09e3b21ca4a92ae2a2d776f4236
Dinesh <dineshvardhandundi@gmail.com>
2026-06-11 22:18:34 +0530

    Update prefix.v

--- RTL/4x4 MAC RTL Files/prefix.v
+++ RTL/4x4 MAC RTL Files/prefix.v
@@ -1,10 +1,10 @@
 `timescale 1ns / 1ps
 
-
-module not_1(a,b
+module prefix(
+    input [1:0] a,b,
+    output [1:0] y
     );
-input a;
-output b;
-assign b=~a;
+ma_3 pp1(b[0],a[1],b[1],y[0]);
+ma_3 pp2(a[0],a[1],b[1],y[1]);
 
 endmodule
